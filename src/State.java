0ec10a48d32f9918470e7426426e0cf089f51837
Keshav Ganapathy <55774025+keshavganapathy@users.noreply.github.com>
2020-02-24 09:56:42 -0500

    Update State.java

--- src/State.java
+++ src/State.java
@@ -1,17 +1,36 @@
+import java.awt.*;
+import java.awt.print.*;
 import java.util.*;
 
 public class State {
 	private ArrayList<Change> modRep;
-	public State(ArrayList<Change>report) {
+	
+	public static void main(String[] args){
+		ArrayList<Change> curry = new ArrayList<Change>();
+		State a = new State(curry);
+		a.print();
+	}
+	
+	public State(ArrayList<Change> report) {
 		modRep = report;
 	}
-	public ArrayList<Change> getModificationReport(){
+
+	public ArrayList<Change> getModificationReport() {
 		return this.modRep;
 	}
+
 	public void save() {
-		getCounter() = getCounter + 1
+
 	}
+
 	public void print() {
-		
+		PrinterJob pj = PrinterJob.getPrinterJob();
+		if (pj.printDialog()) {
+			try {
+				pj.print();
+			} catch (PrinterException exc) {
+				System.out.println(exc);
+			}
+		}
 	}
 }
